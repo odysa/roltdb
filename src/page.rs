1a4ae817f28cee0cd90bbe5fc00ea329629bc820
odysa <piensengxv@gmail.com>
2022-01-07 18:09:23 -0600

    feat: branch ele

--- src/page.rs
+++ src/page.rs
@@ -1,3 +1,5 @@
+use std::slice::from_raw_parts;
+
 use crate::error::{Error, Result};
 
 pub type PageType = u8;
@@ -35,20 +37,53 @@ impl Page {
             Err(Error::InvalidPageType)
         } else {
             unsafe {
-                let meta_ptr = self.ptr as *mut Meta;
+                let meta_ptr = self.ptr as *const Meta;
                 let meta = &*meta_ptr;
                 Ok(meta)
             }
         }
     }
+    pub fn free_list(&self) -> Result<&[PageId]> {
+        if self.page_type != Page::FREE_LIST_PAGE {
+            Err(Error::InvalidPageType)
+        } else {
+            unsafe {
+                let addr = self.ptr as *const PageId;
+                Ok(from_raw_parts(addr, self.count as usize))
+            }
+        }
+    }
+    pub fn branch_elements(&self) -> Result<&[BranchPageElement]> {
+        if self.page_type != Page::BRANCH_PAGE {
+            Err(Error::InvalidPageType)
+        } else {
+            unsafe {
+                let addr = self.ptr as *const u64 as *const BranchPageElement;
+                Ok(from_raw_parts(addr, self.count as usize))
+            }
+        }
+    }
+    pub fn leaf_elements(&self) -> Result<&[LeafPageElement]> {
+        if self.page_type != Page::BRANCH_PAGE {
+            Err(Error::InvalidPageType)
+        } else {
+            unsafe {
+                let addr = self.ptr as *const u64 as *const LeafPageElement;
+                Ok(from_raw_parts(addr, self.count as usize))
+            }
+        }
+    }
 }
 
-struct BranchPageElement {
+#[repr(C)]
+pub struct BranchPageElement {
     pos: u32,
     k_size: u32,
     id: PageId,
 }
-struct LeafPageElement {
+
+#[repr(C)]
+pub struct LeafPageElement {
     pos: u32,
     k_size: u32,
     v_size: u32,
