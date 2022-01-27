ec057fbb1c8c48b541e36345f9100ca2eb47cc4d
odysa <piensengxv@gmail.com>
2022-01-26 20:23:39 -0600

    feat: init db

--- src/db.rs
+++ src/db.rs
@@ -1,9 +1,80 @@
-use std::{rc::Weak, sync::Arc};
+use core::panic;
+use std::{
+    fs::{File, OpenOptions},
+    ops::Deref,
+    path::{Path, PathBuf},
+    rc::{Rc, Weak},
+    sync::{Mutex, RwLock},
+};
+
+use memmap::{Mmap, MmapOptions};
+
+use crate::{error::Result, free_list::FreeList, meta::Meta, page::Page};
 
 #[derive(Debug)]
-pub struct DB(pub Arc<IDB>);
+pub struct DB(pub Rc<IDB>);
 #[derive(Debug)]
 pub struct WeakDB(pub Weak<IDB>);
 
 #[derive(Debug)]
-pub struct IDB {}
+pub struct IDB {
+    mmap: Mmap,
+    file: Mutex<File>,
+    page_size: u64,
+    free_list: FreeList,
+}
+
+impl IDB {
+    pub fn open(file: File) -> Result<Self> {
+        let page_size = page_size::get() as u64;
+
+        let mmap = unsafe {
+            MmapOptions::new()
+                .offset(0)
+                .len(page_size as usize)
+                .map(&file)
+                .map_err(|_| "fail to mmap")?
+        };
+
+        let mut db = IDB {
+            mmap,
+            page_size,
+            file: Mutex::new(file),
+            free_list: FreeList::new(),
+        };
+        let meta = db.meta();
+        let free_list = Page::from_buf(&db.mmap, meta.free_list, page_size)
+            .free_list()
+            .unwrap();
+        if !free_list.is_empty() {
+            db.free_list.init(free_list);
+        }
+        Ok(db)
+    }
+    fn meta(&self) -> Meta {
+        let buf = &self.mmap;
+        let meta0 = Page::from_buf(buf, 0, self.page_size).meta().unwrap();
+        let meta1 = Page::from_buf(buf, 1, self.page_size).meta().unwrap();
+        let meta = match (meta0.validate(), meta1.validate()) {
+            (true, true) => meta0,
+            (true, false) => meta0,
+            (false, true) => meta1,
+            (false, false) => panic!("both meta not valid"),
+        };
+        meta
+    }
+    fn init_file(p: &Path, page_size: u64, page_num: usize) -> Result<File> {
+        let mut file = OpenOptions::new()
+            .create(true)
+            .read(true)
+            .write(true)
+            .open(p)?;
+    }
+}
+
+impl Deref for DB {
+    type Target = IDB;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
