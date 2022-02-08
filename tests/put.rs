80a4a2e8f9b4f7c6ae7f31c39852373f181b8e05
odysa <piensengxv@gmail.com>
2022-02-08 15:23:40 -0600

    ci: add ci

--- tests/put.rs
+++ tests/put.rs
@@ -10,6 +10,7 @@ fn open() {
     }
     let mut b = tx.create_bucket_if_not_exist("test".to_string()).unwrap();
     b.put(b"hello", b"hello world").unwrap();
+    b.put(b"a", b"a").unwrap();
     let res = b.get(b"a").unwrap();
     assert_eq!(res, b"a");
     let res = b.get(b"hello").unwrap();
